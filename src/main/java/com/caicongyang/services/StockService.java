2d6077be2199ab95c8878851160fe608c48c2aa6
caicongyang <80929288@yonghui.cn>
2020-01-28 22:58:05 +0800

    add some todo

--- src/main/java/com/caicongyang/services/StockService.java
+++ src/main/java/com/caicongyang/services/StockService.java
@@ -3,7 +3,7 @@ package com.caicongyang.services;
 public interface StockService {
 
     /**
-     * 当天是否交易
+     * a.当天是否交易
      *
      * @return
      */
@@ -11,10 +11,30 @@ public interface StockService {
 
 
     /**
-     * 捕捉异常股票数据
+     * b. 2：15根据当天成交量  捕捉异常股票数据
      * *
      */
     void catchAbnormalStockData();
+
+
+    /**
+     * c.抓取异常股票的版块分布和概念分布
+     */
+
+
+    /**
+     * d.抓取当天的交易数据
+     */
+
+    /**
+     *  d.19：00 根据当天成交量，捕捉异常股票数据
+     */
+
+
+    /**
+     * f.发送版本或者概念版块具体的股票代码到邮箱
+     *
+     */
 
 
 }
