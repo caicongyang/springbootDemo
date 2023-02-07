f1f87c3ad671fd65a22fda9ca5e16e4b3dcc7b68
caicongyang <caicongyang@gmail.com>
2023-02-07 17:31:53 +0800

    fix bug

--- sklywalking-expand-starter/src/main/java/com/caicongyang/sklywalking/annotation/EnableTraceClient.java
+++ sklywalking-expand-starter/src/main/java/com/caicongyang/sklywalking/annotation/EnableTraceClient.java
@@ -1,5 +1,6 @@
 package com.caicongyang.sklywalking.annotation;
 
+import com.caicongyang.sklywalking.config.SklywalingExpandAutoConfig;
 import com.caicongyang.sklywalking.config.TraceClientConfigurationImportSelector;
 import com.caicongyang.sklywalking.db.mybatis.MybatisTraceSelectInterceptor;
 import com.caicongyang.sklywalking.db.mybatis.MybatisTraceUpdateInterceptor;
@@ -11,7 +12,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import({TraceClientConfigurationImportSelector.class, MybatisTraceSelectInterceptor.class, MybatisTraceUpdateInterceptor.class})
+@Import({TraceClientConfigurationImportSelector.class, MybatisTraceSelectInterceptor.class, MybatisTraceUpdateInterceptor.class, SklywalingExpandAutoConfig.class})
 public @interface EnableTraceClient {
 
     String[] includePatterns() default {"/**/**"};
