4c5c168cea0b8007daa9e12ff80a141d5f6f40df
caicongyang <caicongyang@gmail.com>
2023-02-07 22:31:02 +0800

    获取应用名称

--- cache-starter/src/main/java/com/caicongyang/cache/RedisCache.java
+++ cache-starter/src/main/java/com/caicongyang/cache/RedisCache.java
@@ -1,5 +1,6 @@
 package com.caicongyang.cache;
 
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.Set;
@@ -9,32 +10,35 @@ public class RedisCache implements Cache {
     public RedisTemplate<Object, Object> template;
 
 
-    public RedisCache(RedisTemplate<Object, Object> redisTemplate) {
+    public String prefix;
+
+    public RedisCache(RedisTemplate<Object, Object> redisTemplate,Environment environment) {
         this.template = redisTemplate;
+        this.prefix = environment.getProperty("spring.application.name")+":";
     }
 
 
     @Override
     public Object get(String key) {
-        return  template.opsForValue().get(key);
+        return template.opsForValue().get(prefix+key);
     }
 
     @Override
     public void set(String key, Object value) {
-        template.opsForValue().set(key, value);
+        template.opsForValue().set(prefix+key, value);
     }
 
     @Override
     public void add4Set(String key, Object value) {
-        if (template.opsForSet().size(key) > 1024) {
+        if (template.opsForSet().size(prefix+key) > 1024) {
             throw new RuntimeException("set集合大小超过限制！");
         }
-        template.opsForSet().add(key, value);
+        template.opsForSet().add(prefix+key, value);
     }
 
     @Override
     public Set<Object> members4Set(String key) {
-        Set<Object> members = template.opsForSet().members(key);
+        Set<Object> members = template.opsForSet().members(prefix+key);
         return members;
     }
 }
--- cache-starter/src/main/java/com/caicongyang/cache/config/CacheAutoConfiguration.java
+++ cache-starter/src/main/java/com/caicongyang/cache/config/CacheAutoConfiguration.java
@@ -4,14 +4,15 @@ import com.caicongyang.cache.RedisCache;
 import com.caicongyang.cache.RedisProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class CacheAutoConfiguration {
 
     @Bean
-    public RedisCache redisCache(RedisTemplate<Object, Object> redisTemplate) {
-        return new RedisCache(redisTemplate);
+    public RedisCache redisCache(RedisTemplate<Object, Object> redisTemplate, Environment environment) {
+        return new RedisCache(redisTemplate,environment);
     }
 
 
