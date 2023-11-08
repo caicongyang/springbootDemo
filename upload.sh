62804cd1603f42036da4e01487e6c460ab2d4b16
caicongyang <caicongyang@gmail.com>
2023-11-08 18:03:23 +0800

    add some code

--- upload.sh
+++ upload.sh
@@ -3,5 +3,6 @@
 mvn clean install -Dmaven.test.skip=true
 
 cd stock/target
-scp stock-0.0.1-SNAPSHOT.jar root@49.235.178.21:/data/java
+scp stock-0.0.1-SNAPSHOT.jar  root@124.221.175.114:/opt/data/
+
 
